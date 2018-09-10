2d24ec8efd2bbd89f59620c7f9353c23988d13a3
Tomasz Sterna <tomek@xiaoka.com>
2018-09-10 07:40:13 +0200

    Limit returned items from selector

--- src/state/selectors.js
+++ src/state/selectors.js
@@ -4,13 +4,15 @@ import { createSelector } from 'reselect'
 import { selectAllItems } from './ducks/items'
 import { selectCategoryFilters, selectColorFilters } from './ducks/filters'
 
+const MAX_RETURNED_ITEMS = 42
+
 export const selectItemsFiltered = createSelector(
   [selectCategoryFilters, selectColorFilters, selectAllItems],
   (categoriesFilter, colorsFilter, allItems) => {
     const items = []
     allItems.filter(item => categoriesFilter.length === 0 || categoriesFilter.includes(item.category)).forEach((item) => {
       Object.keys(item.colors).forEach((color) => {
-        if (colorsFilter.length === 0 || colorsFilter.includes(color)) {
+        if (items.length < MAX_RETURNED_ITEMS && (colorsFilter.length === 0 || colorsFilter.includes(color))) {
           items.push({
             ...item,
             color,
